60ea804b825fa71a6971e7e99170267aa91c61ca
Miroslav Ljubicic <127101426+mrkyns@users.noreply.github.com>
2023-06-21 10:39:25 +0200

    added graphics, refracture css in landing page and login page

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,28 @@
+import "./loader.css";
+import LogoIcon from "./LogoIcon";
+import Typewriter from "typewriter-effect";
+
+export default function Loader() {
+  return (
+    <div className="loader">
+      <LogoIcon />
+      <div class="spinner"></div>
+      <h1 className="loader_txt">
+        <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .changeDelay(75)
+              .typeString("Data Loading ...")
+              .pauseFor(1000)
+              .deleteAll()
+              .start();
+          }}
+        />
+      </h1>
+    </div>
+  );
+}
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,18 +1,19 @@
 import "./RegisterPage.css";
 import LogoHorizontal from "../../components/LogoHorizontal";
 import Button from "../../components/Button";
-import { Link, NavLink } from "react-router-dom"
-import * as yup from "yup"
-import { Formik, Form, Field } from "formik"
+import { Link, NavLink } from "react-router-dom";
+import * as yup from "yup";
+import { Formik, Form, Field } from "formik";
 
 const registerSchema = yup.object().shape({
-  name: yup.string().required('Full name is required'),
-  userName: yup.string().required('Username is required'),
-  email: yup.string().email('Invalid email').required('Email is required'),
-  password: yup.string().required('Password is required'),
-  confirmPassword: yup.string()
-    .oneOf([yup.ref('password'), null], 'Passwords must match')
-    .required('Confirm Password is required'),
+  name: yup.string().required("Full name is required"),
+  userName: yup.string().required("Username is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup.string().required("Password is required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .required("Confirm Password is required"),
 });
 
 const initialRegisterValues = {
@@ -20,17 +21,16 @@ const initialRegisterValues = {
   userName: "",
   email: "",
   password: "",
-  confirmPassword: ""
-}
+  confirmPassword: "",
+};
 
 export default function RegisterPage() {
-
   const register = async (values) => {
     try {
-      const response = await fetch('http://localhost:8080/user/signup', {
-        method: 'POST',
+      const response = await fetch("http://localhost:8080/user/signup", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(values),
       });
@@ -38,10 +38,10 @@ export default function RegisterPage() {
       if (response.ok) {
         const registeredUser = await response.json();
       } else {
-        console.log('Error occurred while registering');
+        console.log("Error occurred while registering");
       }
     } catch (error) {
-      console.error('Error occurred while registering', error);
+      console.error("Error occurred while registering", error);
     }
   };
 
@@ -73,59 +73,50 @@ export default function RegisterPage() {
               validationSchema={registerSchema}
               onSubmit={handleFormSubmit}
             >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-              submitForm
-              /* and other goodies */
-            }) => (
-              <Form onSubmit={handleSubmit}>
-                <Field
-                  type="text"
-                  placeholder="full name"
-                  name="name"
-                />
-                <Field
-                  type="text"
-                  placeholder="username"
-                  name="userName"
-                />
-                <Field
-                  type="text"
-                  placeholder="email"
-                  name="email"
-                />
-                <Field
-                  type="text"
-                  placeholder="password"
-                  name="password"
-                />
-                <Field
-                  type="text"
-                  placeholder="confirm password"
-                  name="confirmPassword"
-                />
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                submitForm,
+                /* and other goodies */
+              }) => (
+                <Form onSubmit={handleSubmit}>
+                  <Field type="text" placeholder="full name" name="name" />
+                  <Field type="text" placeholder="username" name="userName" />
+                  <Field type="text" placeholder="email" name="email" />
+                  <Field
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                  />
+                  <Field
+                    type="password"
+                    placeholder="confirm password"
+                    name="confirmPassword"
+                  />
 
-                <div className="registerpage__footer">
-                <p>
-                  By registering, you are agreeing to our Terms of Use and Privacy
-                  Policy
-                </p>
-                  <Link to="/initialsetup" >
-                    <Button txt={"Register"} func={() => handleSubmitButtonClick(submitForm)} />
-                  </Link>
-                  <div className="registerpage__footer-bottom">
-                    <p>Already have an account?</p>
-                    <NavLink to="/login">Login</NavLink>
+                  <div className="registerpage__footer">
+                    <p>
+                      By registering, you are agreeing to our Terms of Use and
+                      Privacy Policy
+                    </p>
+                    <Link to="/initialsetup">
+                      <Button
+                        txt={"Register"}
+                        func={() => handleSubmitButtonClick(submitForm)}
+                      />
+                    </Link>
+                    <div className="registerpage__footer-bottom">
+                      <p>Already have an account?</p>
+                      <NavLink to="/login">Login</NavLink>
+                    </div>
                   </div>
-                </div>
                 </Form>
-            )}
+              )}
             </Formik>
           </div>
         </div>
