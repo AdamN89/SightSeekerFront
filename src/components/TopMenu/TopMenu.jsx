937699f2d717b035e7f9b470928306c7a804c647
Adam <adamna1989@gmail.com>
2023-06-22 12:54:21 +0100

    to menu with animation done

--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -8,26 +8,42 @@ import ChatIcon from "./Icons/ChatIcon"
 import SettingsIcon from "./Icons/SettingsIcon"
 import AboutIcon from "./Icons/AboutIcon"
 import CloseIcon from "./Icons/CloseIcon";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TopMenu() {
   const [isOpen, setIsOpen ] = useState(false)
+  const [ isClosing, setIsClosing ] = useState(false)
+  const menuRef = useRef(null)
 
     const openMenu = () => {
+      menuRef.current.classList.add("opening")
       setIsOpen(!isOpen)
+      // setIsClosing(false)
+      setTimeout(() => {
+        menuRef.current.classList.remove("opening")
+      }, 500)
       console.log("click is working")
     }
+
+    const closeMenu = () => {
+      menuRef.current.classList.add("closing")
+      setTimeout(() => {
+        setIsOpen(!isOpen)
+        menuRef.current.classList.remove("closing")
+      }, 700)
+    }
+
 
   return (
     <div className="topmenu__wrapper">
         <div className="topmenu__topbar">
             <LogoHorizontal />
-            <button onClick={openMenu} style={{ zIndex: isOpen ? 1: 3 }}><MenuIcon /></button>
+            <button onClick={openMenu} style={{ zIndex: isOpen ? 0: 3 }}><MenuIcon /></button>
         </div>
-        <div className="topmenu__body-wrapper" style={{ opacity: isOpen ? 2 : 0 }}>
+        <div className="topmenu__body-wrapper" ref={menuRef} style={{ opacity: isOpen ? 2 : 0 }}>
           <div className="topmenu__body_header">
             <img src="../../assets/defaultavatar/09.jpg" alt="user-img" />
-            <button onClick={openMenu}><CloseIcon /></button>
+            <button onClick={closeMenu}><CloseIcon /></button>
           </div>
           <div className="topmenu__body_body">
             <p><TravelPlan />TravelPlan</p>
