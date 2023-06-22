f5efb6c68cdb99791ecdf2caed2c309c26408bc1
Miroslav Ljubicic <127101426+mrkyns@users.noreply.github.com>
2023-06-22 14:57:40 +0200

    corrected classes for delete button

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,11 +1,12 @@
+import "./AboutPage.css";
 import TopMenu from "../../components/TopMenu/TopMenu";
+import ButtonDelete from "../../components/ButtonDelete";
 
 export default function About() {
   return (
     <div>
-      <TopMenu />
-      
+      {/* <TopMenu /> */}
+      <ButtonDelete txt={"some  text"} />
     </div>
-  )
-};
-
+  );
+}
