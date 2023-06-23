5ee95da272b9649d642f80bf1cd8190a0c89f778
Adam <adamna1989@gmail.com>
2023-06-23 09:07:21 +0100

    cleaned up about page to avoid conflicts

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,35 +1,7 @@
-import TopMenu from "../../components/TopMenu/TopMenu";
-
 export default function About() {
   return (
     <div>
-      <TopMenu />
-      <div>
-        <p>This is just a test</p>
-        <p>This is just a test</p>
-        <p>This is just a test</p>
-      </div>
-      <div>
-        <p>This is just a test</p>
-        <p>This is just a test</p>
-        <p>This is just a test</p>
-      </div>
-      <div>
-        <p>This is just a test</p>
-        <p>This is just a test</p>
-        <p>This is just a test</p>
-      </div>
-      <div>
-        <p>This is just a test</p>
-        <p>This is just a test</p>
-        <p>This is just a test</p>
-      </div>
-      <div>
-        <p>This is just a test</p>
-        <p>This is just a test</p>
-        <p>This is just a test</p>
-      </div>
-      v
+
     </div>
   )
 };
