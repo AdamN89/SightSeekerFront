5d3ea867d4d17805535c6e58652ed563c4c469c9
Adam <adamna1989@gmail.com>
2023-06-28 16:08:12 +0100

    working on displaying multiple members

--- src/pages/Chat/Conversation.jsx
+++ src/pages/Chat/Conversation.jsx
@@ -5,19 +5,56 @@ export default function Conversation({ data, currentUserId, online, chat, setSen
 
     const [ userData, setUserData ] = useState(null) // This is who we send the messages to
 
-    useEffect(() => {
-        const userId = data.members.find((id) => id !== currentUserId)
-        const getUserData = async() => {
-            try {
-                const response = await fetch(`http://localhost:8080/user/${userId}`)
-                const data = await response.json()
-                setUserData(data.data)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        getUserData()
-    },[])
+  useEffect(() => {
+    const userId = data.members.find((id) => id !== currentUserId)
+    console.log("this is userId", userId)
+
+    console.log("this is chatrooms members", data.members)
+
+    const newFunc = () => {
+    if (data.members[1] > 1) {
+      console.log("THIS IS JUST ONE ENDING 6c4e",data._id)
+    }}
+
+    const getUserData = async() => {
+      try {
+        const response = await fetch(`http://localhost:8080/user/${userId}`)
+        const data = await response.json()
+        console.log("this is fetching chat members",data.data.name)
+        setUserData(data.data)
+    } catch (error) {
+        console.log(error)
+    }
+    }
+    getUserData()
+
+    
+    // const filteredMembers = data.members.filter(member => member !== currentUserId)
+    // console.log("filtered members", filteredMembers)
+    // const chatMember = filteredMembers.map((member) => member)
+    // console.log("this is chatmember", chatMember)
+
+    // data.members[1].forEach(member => console.log(member.find((id) => id !== currentUserId)))
+    
+    // chatMember.forEach((member) => {
+    //   console.log("this is id", member)
+    //     const getUserData = async() => {
+    //     try {
+    //       const response = await fetch(`http://localhost:8080/user/${member}`)
+    //       const data = await response.json()
+    //       console.log("this is fetching chat members",data.data.name)
+    //       setUserData(data.data)
+    //   } catch (error) {
+    //       console.log(error)
+    //   }
+    //   }
+    //   getUserData()
+    // }
+    //   )
+    
+        newFunc()
+  },[])
+    
 
   return (
     <>
